b67e170b4ead64841b9dabc25b4006c76d771503
anaclaraferreirajoaquim <anaclaraferreirajoaquim@gmail.com>
2023-08-28 18:14:55 +0000

    testes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash, redirect 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def base():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+if (__name__) == "__main__":
+   app.run(port="5100", debug="True") 
